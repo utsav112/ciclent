bd89337fc05e0aff637322f29cd28407edfa4094
utsav112 <132879466+utsav112@users.noreply.github.com>
2023-05-27 17:37:41 +0530

    Add files via upload

---
+++ c-17p.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+main()
+
+{
+	int i,j,k;
+	for(i=1;i<=5;i++)
+	{
+		for(k=i;k<=5;k++)
+		{
+			printf(" ");
+		}
+		for(j=1;j<=i;j++)
+		{
+			printf("%d",j);
+		}
+		printf("\n");
+	}
+	for(i=2;i<=5;i++)
+	{
+		for(k=i;k>=1;k--)
+		{
+			printf(" ");
+		}
+		for(j=5;j<=i;j++)
+		{
+			printf("%d",i);
+		}
+		printf("\n");
+	}
+}
